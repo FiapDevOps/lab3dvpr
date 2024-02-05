2afa7052a5a177307fff38ad1eb554b8e19d0258
Helder Pereira <helcorintho@gmail.com>
2024-02-04 21:09:20 -0300

    Update output.tf

--- prometheus/terraform/output.tf
+++ prometheus/terraform/output.tf
@@ -2,15 +2,5 @@
 
 output "prometheus_public_ip" {
   value = aws_instance.monitoring_app.public_ip
-  description = "The prometheus public ip address"
+  description = "The prometheus stack public ip address"
 }
-
-output "prometheus_private_ip" {
-  value = aws_instance.monitoring_app.private_ip
-  description = "The prometheus private ip address"
-}
-
-#output "single_app_addr" {
-#  value = aws_instance.single_app[0].public_ip
-#  description = "The mediawiki public ip address"
-#}
