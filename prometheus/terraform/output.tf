806269ad30b29e5020f5d0c016caa928d815231f
Helder Pereira <helcorintho@gmail.com>
2024-02-04 21:13:54 -0300

    Update output.tf - Adicionar entrada de DNS publica

--- prometheus/terraform/output.tf
+++ prometheus/terraform/output.tf
@@ -4,3 +4,8 @@ output "prometheus_public_ip" {
   value = aws_instance.monitoring_stack_lab.public_ip
   description = "The prometheus stack public ip address"
 }
+
+output "prometheus_public_dns" {
+  value = aws_instance.monitoring_stack_lab.public_dns
+  description = "The prometheus stack public dns address"
+}
