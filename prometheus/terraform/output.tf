b8a25e73ede1e6c938f33d86bba4f5f8970d8fbd
Helder Pereira <helcorintho@gmail.com>
2024-02-04 21:10:35 -0300

    Update output.tf

--- prometheus/terraform/output.tf
+++ prometheus/terraform/output.tf
@@ -1,6 +1,6 @@
 # https://www.terraform.io/language/values/outputs
 
 output "prometheus_public_ip" {
-  value = aws_instance.monitoring_app.public_ip
+  value = aws_instance.monitoring_stack_lab.public_ip
   description = "The prometheus stack public ip address"
 }
